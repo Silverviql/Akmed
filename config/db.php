f93d9a52815f03a07e0824065c3ee6215908496a
Khabibulin Emil <khabibulinemil@gmail.com>
2017-04-25 17:06:39 +0300

    Подключение к БД

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=akmed',
+    'dsn' => 'mysql:host=localhost;dbname=post',
     'username' => 'root',
     'password' => '11031971',
     'charset' => 'utf8',
