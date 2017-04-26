ffba8ee6ab56c332e434cc3f0ace543ec558511e
Khabibulin Emil <khabibulinemil@gmail.com>
2017-04-26 11:35:16 +0300

    Тест скрипта

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;port=3306;dbname=akmed',
-    'username' => 'root',
+    'username' => 'silverfire',
     'password' => '11031971',
     'charset' => 'utf8',
 ];
