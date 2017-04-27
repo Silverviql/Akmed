a5b6809b55083c6540feab3bc63b37fa6d140b42
Khabibulin Emil <khabibulinemil@gmail.com>
2017-04-27 14:30:24 +0300

    test

--- views/site/contact.php
+++ views/site/contact.php
@@ -1,78 +1,5 @@
 <?php
 
-
-/*use yii\helpers\Html;
-use yii\bootstrap\ActiveForm;
-use yii\captcha\Captcha;
-
-$this->title = 'Contact';
-$this->params['breadcrumbs'][] = $this->title;
-?>
-<div class="site-contact">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <?php if (Yii::$app->session->hasFlash('contactFormSubmitted')): ?>
-
-        <div class="alert alert-success">
-            Thank you for contacting us. We will respond to you as soon as possible.
-        </div>
-
-        <p>
-            Note that if you turn on the Yii debugger, you should be able
-            to view the mail message on the mail panel of the debugger.
-            <?php if (Yii::$app->mailer->useFileTransport): ?>
-                Because the application is in development mode, the email is not sent but saved as
-                a file under <code><?= Yii::getAlias(Yii::$app->mailer->fileTransportPath) ?></code>.
-                                                                                                    Please configure the <code>useFileTransport</code> property of the <code>mail</code>
-                application component to be false to enable email sending.
-            <?php endif; ?>
-        </p>
-
-    <?php else: ?>
-
-        <p>
-            If you have business inquiries or other questions, please fill out the following form to contact us.
-            Thank you.
-        </p>
-
-        <div class="row">
-            <div class="col-lg-5">
-
-                <?php if( Yii::$app->session->hasFlash('success') ): ?>
-                    <div class="alert alert-success alert-dismissible" role="alert">
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                        <?php echo Yii::$app->session->getFlash('success'); ?>
-                    </div>
-                <?php endif;?>
-
-                <?php if( Yii::$app->session->hasFlash('error') ): ?>
-                    <div class="alert alert-danger alert-dismissible" role="alert">
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                        <?php echo Yii::$app->session->getFlash('error'); ?>
-                    </div>
-                <?php endif;?>
-
-                <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
-
-                <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
-
-                <?= $form->field($model, 'email') ?>
-
-                <?= $form->field($model, 'subject') ?>
-
-                <?= $form->field($model, 'text')->textarea(['rows' => 6]) ?>
-
-
-                <?= Html::submitButton('Отправить', ['class' => 'btn btn-success'])?>
-
-                <?php ActiveForm::end(); ?>
-
-            </div>
-        </div>
-
-    <?php endif; ?>
-</div>*/
-
 /* @var $this \yii\web\View */
 /* @var $content string */
 use yii\helpers\Html;
@@ -474,7 +401,7 @@ $this->params['breadcrumbs'][] = $this->title;
                         <?php ActiveForm::end(); ?>
                   </div>
         </div>
-    </div>
+
                         <h3 class="cnt-ttl">Having Any Query! Or Book an appointment</h3>
                         <div class="space"></div>
                         <div id="sendmessage">Your message has been sent. Thank you!</div>
