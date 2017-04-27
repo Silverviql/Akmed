e81faefc28292d344abefd91327194a32588cf08
Khabibulin Emil <khabibulinemil@gmail.com>
2017-04-27 14:34:31 +0300

    test

--- views/site/contact.php
+++ views/site/contact.php
@@ -405,21 +405,21 @@ $this->params['breadcrumbs'][] = $this->title;
                         <div class="space"></div>
                         <div id="sendmessage">Your message has been sent. Thank you!</div>
                         <div id="errormessage"></div>
-                        <form action="" method="post" role="form" class="contactForm">
+                        <form action="" method="post" role="form" class="ContactForm">
                             <div class="form-group">
-                                <input type="text" name="name" class="form-control br-radius-zero" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                                <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
                                 <div class="validation"></div>
                             </div>
                             <div class="form-group">
-                                <input type="email" class="form-control br-radius-zero" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                                <?= $form->field($model, 'email') ?>
                                 <div class="validation"></div>
                             </div>
                             <div class="form-group">
-                                <input type="text" class="form-control br-radius-zero" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                                <?= $form->field($model, 'subject') ?>
                                 <div class="validation"></div>
                             </div>
                             <div class="form-group">
-                                <textarea class="form-control br-radius-zero" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+                                <?= $form->field($model, 'text')->textarea(['rows' => 6]) ?>
                                 <div class="validation"></div>
                             </div>
 
