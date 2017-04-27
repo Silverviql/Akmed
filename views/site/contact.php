98d61fdbf8ddc01c0cac15ac147cd572b6358b11
Khabibulin Emil <khabibulinemil@gmail.com>
2017-04-27 10:41:17 +0300

    Новая валидация

--- views/site/contact.php
+++ views/site/contact.php
@@ -6,6 +6,7 @@
 
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
+use yii\widgets\ActiveForm;
 use yii\captcha\Captcha;
 
 $this->title = 'Contact';
@@ -41,6 +42,21 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="row">
             <div class="col-lg-5">
 
+                <?php if( Yii::$app->session->hasFlash('success') ): ?>
+                    <div class="alert alert-success alert-dismissible" role="alert">
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <?php echo Yii::$app->session->getFlash('success'); ?>
+                    </div>
+                <?php endif;?>
+
+                <?php if( Yii::$app->session->hasFlash('error') ): ?>
+                    <div class="alert alert-danger alert-dismissible" role="alert">
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <?php echo Yii::$app->session->getFlash('error'); ?>
+                    </div>
+                <?php endif;?>
+
+
                 <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
 
                 <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
@@ -56,6 +72,8 @@ $this->params['breadcrumbs'][] = $this->title;
                     <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
                 </div>
 
+                <?= Html::submitButton('Отправить', ['class' => 'btn btn-success'])?>
+
                 <?php ActiveForm::end(); ?>
 
             </div>
@@ -63,3 +81,4 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <?php endif; ?>
 </div>
+
