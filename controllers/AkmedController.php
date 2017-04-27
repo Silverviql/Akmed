b7d2ad584aad3897d39305e1574e2435fc20c50f
Khabibulin Emil <khabibulinemil@gmail.com>
2017-04-27 13:05:55 +0300

    Новый контроллер Акмед

---
+++ controllers/AkmedController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use app\models\LoginForm;
+use app\models\ContactForm;
+
+class SiteController extends Controller
+{
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['logout'],
+                'rules' => [
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('index');
+
+
+
+        $connection = Yii::$app->db;
+        if ($connection)
+            echo "Есть подключение к БД";
+        else
+            echo "Нет подключения к БД";
+
+        Yii::$app->db->createCommand('SELECT 1')->execute();
+    }
+
+
+    /*public function actionContact()
+    {
+        $model = new ContactForm();
+
+        if ( $model->load(Yii::$app->request->post()) ) {
+            if ($model->save()) {
+                Yii::$app->session->setFlash('success', 'Данные приняты');
+                return $this->refresh();
+            } else {
+                Yii::$app->session->setFlash('error', 'Ошибка');
+            }
+        }
+        $this->view->title = 'Все статьи';
+        return $this->render('contact', compact('model'));
+
+
+
+    }*/
+
+
+
+}
