8895fcfa03efdfa37ba49b94f36580b205b45034
Khabibulin Emil <khabibulinemil@gmail.com>
2017-04-27 14:20:00 +0300

    test

--- views/site/contact.php
+++ views/site/contact.php
@@ -4,7 +4,7 @@
 /* @var $form yii\bootstrap\ActiveForm */
 /* @var $model app\models\ContactForm */
 
-use yii\helpers\Html;
+/*use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 use yii\captcha\Captcha;
 
@@ -74,7 +74,7 @@ $this->params['breadcrumbs'][] = $this->title;
         </div>
 
     <?php endif; ?>
-</div>
+</div>*/
 
 <?php
 /* @var $this \yii\web\View */
