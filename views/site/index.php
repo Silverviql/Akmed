892394a22c0a01f89fefa103a7236626e83e2e93
Khabibulin Emil <khabibulinemil@gmail.com>
2017-04-28 11:29:50 +0300

    подключаем use yii\bootstrap\ActiveForm;

--- views/site/index.php
+++ views/site/index.php
@@ -7,7 +7,9 @@ use yii\helpers\Html;
 use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
-use app\assets\AppAsset;
+use app\assets\AppAsset;;
+use yii\bootstrap\ActiveForm;
+
 
 AppAsset::register($this);
 ?>
