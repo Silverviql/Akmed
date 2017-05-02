8ae3e059a78c7ad45709e874ad25b92105c02842
Khabibulin Emil <khabibulinemil@gmail.com>
2017-05-02 13:04:24 +0300

    старый дизайн

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -57,12 +57,28 @@ AppAsset::register($this);
     ?>
 
     <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= $content ?>
+        <div class="col-md-12">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#"><img src="img/logo.png" class="img-responsive" style="width: 140px; margin-top: -16px;"></a>
+            </div>
+            <div class="collapse navbar-collapse navbar-right" id="myNavbar">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#banner">Home</a></li>
+                    <li class=""><a href="#service">Services</a></li>
+                    <li class=""><a href="#about">About</a></li>
+                    <li class=""><a href="#testimonial">Testimonial</a></li>
+                    <li class=""><a href="#contact">Contact</a></li>
+                </ul>
+            </div>
+        </div>
     </div>
 </div>
+
 
 <footer class="footer">
     <div class="container">
