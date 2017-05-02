5f858fdc37da95a1836bbf25a07270549a9b464b
Khabibulin Emil <khabibulinemil@gmail.com>
2017-05-02 11:34:11 +0300

    старый дизайн

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -29,7 +29,7 @@ AppAsset::register($this);
       NavBar::begin([
       //  'brandLabel' => 'My Company',
         'brandUrl' => Yii::$app->homeUrl,
-     //   'options' => [
+        'options' => [
            'class' => 'navbar-inverse navbar-fixed-top',
         ],
     ]);
