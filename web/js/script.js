166783a2939a2b65d2e4b218a1a90e0bed79473e
Emil Khabibulin <khabibulinemil@gmail.com>
2017-05-02 19:44:07 +0300

    Анимация кнопки

--- web/js/script.js
+++ web/js/script.js
@@ -1,5 +1,79 @@
 $(document).ready(function(){
 
+    var p = $('.window_wrap');
+    $('.telButton').click(function() {
+
+        p.css({'display':'block'}).hide().fadeIn(1000);
+
+    });
+
+
+    p.click(function(event) {
+
+        if(event.target == this) {
+            $(this).css({'display':'none'});
+        }
+
+    });
+
+    $('.window_close').click(function() {
+        p.css({'display':'none'});
+    });
+
+
+    $('#telButton').click(function(event) {
+
+        event.preventDefault();
+
+        var tel = $('#telForm').val();
+
+        $('#backPhone').fadeOut(500,function() {
+
+            $('<p>Отправка!</p>').appendTo($('.window')).hide().fadeIn(300,function() {
+
+                $.ajax({
+
+                    type: 'POST',
+                    url: 'srv.php',
+                    data: 'tel=' + tel,
+                    dataType: 'json',
+                    success : function (json) {
+                        if(json.error) {
+                            $('.window p').last().remove();
+                            $('#backPhone').fadeIn(300, function() {
+                                alert(json.error);
+                            });
+                        }
+                        else {
+                            $('.window p').last().fadeOut(300, function() {
+
+                                $(this).text('Заявка принята!').fadeIn(300, function () {
+
+                                    $('.window_wrap').delay(1500).fadeOut(300);
+
+                                });
+
+                            });
+                        }
+                    }
+
+                });
+
+            });
+
+        });
+
+    });
+
+
+
+
+
+
+
+
+
+
 
     $('.telButton .telButton_background').hover(
         function() {
