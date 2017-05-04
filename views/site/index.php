ec4ff33b475eebf579544f675b551fdfc9a840bf
Khabibulin Emil <khabibulinemil@gmail.com>
2017-05-04 14:29:17 +0300

    Исправление относительного путя картинок на абсалютный

--- views/site/index.php
+++ views/site/index.php
@@ -42,7 +42,7 @@ AppAsset::register($this);
                                 <span class="icon-bar"></span>
                                 <span class="icon-bar"></span>
                             </button>
-                            <a class="navbar-brand" href="#"><img src="img/logo.png" class="img-responsive" style="width: 140px; margin-top: -16px;"></a>
+                            <a class="navbar-brand" href="#"><img src="/img/logo.png" class="img-responsive" style="width: 140px; margin-top: -16px;"></a>
                         </div>
                         <div class="collapse navbar-collapse navbar-right" id="myNavbar">
                             <ul class="nav navbar-nav">
@@ -60,7 +60,7 @@ AppAsset::register($this);
                 <div class="row">
                     <div class="banner-info">
                         <div class="banner-logo text-center">
-                            <img src="img/logo.png" class="img-responsive">
+                            <img src="/img/logo.png" class="img-responsive">
                         </div>
                         <div class="banner-text text-center">
                             <h1 class="white"><?= Yii::t('main', 'Healthcare at your desk!!')?></h1>
@@ -221,7 +221,7 @@ AppAsset::register($this);
                 </div>
                 <div class="col-md-3 col-sm-3 col-xs-6">
                     <div class="thumbnail">
-                        <img src="img/doctor1.jpg" alt="..." class="team-img">
+                        <img src="/img/doctor1.jpg" alt="..." class="team-img">
                         <div class="caption">
                             <h3><?= Yii::t('main', 'Jessica Wally')?></h3>
                             <p><?= Yii::t('main', 'Doctor')?></p>
@@ -235,7 +235,7 @@ AppAsset::register($this);
                 </div>
                 <div class="col-md-3 col-sm-3 col-xs-6">
                     <div class="thumbnail">
-                        <img src="img/doctor2.jpg" alt="..." class="team-img">
+                        <img src="/img/doctor2.jpg" alt="..." class="team-img">
                         <div class="caption">
                             <h3>Iai Donas</h3>
                             <p>Doctor</p>
@@ -249,7 +249,7 @@ AppAsset::register($this);
                 </div>
                 <div class="col-md-3 col-sm-3 col-xs-6">
                     <div class="thumbnail">
-                        <img src="img/doctor3.jpg" alt="..." class="team-img">
+                        <img src="/img/doctor3.jpg" alt="..." class="team-img">
                         <div class="caption">
                             <h3>Amanda Denyl</h3>
                             <p>Doctor</p>
@@ -263,7 +263,7 @@ AppAsset::register($this);
                 </div>
                 <div class="col-md-3 col-sm-3 col-xs-6">
                     <div class="thumbnail">
-                        <img src="img/doctor4.jpg" alt="..." class="team-img">
+                        <img src="/img/doctor4.jpg" alt="..." class="team-img">
                         <div class="caption">
                             <h3>Jason Davis</h3>
                             <p>Doctor</p>
@@ -294,7 +294,7 @@ AppAsset::register($this);
                     </div>
                     <div class="testi-info">
                         <!-- User Image -->
-                        <a href="#"><img src="img/thumb.png" alt="" class="img-responsive"></a>
+                        <a href="#"><img src="/img/thumb.png" alt="" class="img-responsive"></a>
                         <!-- User Name -->
                         <h3><?= Yii::t('main', 'Alex')?><span><?= Yii::t('main', 'Texas')?></span></h3>
                     </div>
@@ -306,7 +306,7 @@ AppAsset::register($this);
                     </div>
                     <div class="testi-info">
                         <!-- User Image -->
-                        <a href="#"><img src="img/thumb.png" alt="" class="img-responsive"></a>
+                        <a href="#"><img src="/img/thumb.png" alt="" class="img-responsive"></a>
                         <!-- User Name -->
                         <h3>Alex<span>Texas</span></h3>
                     </div>
@@ -318,7 +318,7 @@ AppAsset::register($this);
                     </div>
                     <div class="testi-info">
                         <!-- User Image -->
-                        <a href="#"><img src="img/thumb.png" alt="" class="img-responsive"></a>
+                        <a href="#"><img src="/img/thumb.png" alt="" class="img-responsive"></a>
                         <!-- User Name -->
                         <h3>Alex<span>Texas</span></h3>
                     </div>
@@ -396,8 +396,8 @@ AppAsset::register($this);
 
                                     <?= $form->field($model, 'subject') ?>
 
-                                    <?= $form->field($model, 'text')->textarea(['rows' => 6]) ?>
 
+                                    <?= $form->field($model, 'text')->textarea(['rows' => 6]) ?>
 
                                     <?= Html::submitButton('Отправить', ['class' => 'btn btn-success'])?>
 
