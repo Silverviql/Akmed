da86f84baca9ec37de7709d5e48cd64aaab1888a
Khabibulin Emil <khabibulinemil@gmail.com>
2017-06-01 13:08:35 +0300

    Перемещение языкового бара.

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -36,7 +36,7 @@ AppAsset::register($this);
                                     <li class=""><a href="#testimonial"><?= Yii::t('main', 'Testimonial')?></a></li>
                                     <li class=""><a href="#contact"><?= Yii::t('main', 'Contact')?></a></li>
                                     <li class=""><a href="http://akmed8294.cloudapp.net/site/application"><?= Yii::t('main', 'Application')?></a></li>
-                                    <li class="language-brand"><img src="/img/language.png" class="img-responsive" style="width: 140px; margin-top: -16px;"><a href="http://akmed8294.cloudapp.net/site/application"></a></li>
+                                    <li class="language-brand"><img src="/img/language.png" class="img-responsive" style="width: 140px; margin-top: -16px;"><?= \app\widgets\LangWidget::widget();?></li>
                                 </ul>
                             </div>
                         </div>
@@ -61,7 +61,6 @@ AppAsset::register($this);
                 </div>
             </div>
         </section>
-        <?= \app\widgets\LangWidget::widget();?>
         <!--/ banner-->
 
 
