a028aae05314356a499cb2a49c8c244a7ce3acfe
Emil Khabibulin <khabibulinemil@gmail.com>
2017-07-27 10:08:54 +0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=127.0.0.1;port=3306;dbname=akmed',
     'username' => 'root',
-    'password' => '11031971',
+    'password' => '12345678',
     'charset' => 'utf8',
 ];
 
